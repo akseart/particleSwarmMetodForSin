9f83c341ba3c70d3033a984c1f6a5ebbe65ce9cc
akseart <akseart@ya.ru>
2018-05-27 19:10:45 +0300

    Graphics

--- particleSwarmMetodForSin/main.cpp
+++ particleSwarmMetodForSin/main.cpp
@@ -4,24 +4,28 @@
 #include <GLUT/GLUT.h>
 #include <time.h>
 #include <unistd.h>
-using namespace std;
 
+#define eps pow(10,-6)
+const int nParticle = 50 ;
+const int nCoordinates =3 ;
+using namespace std;
 class Particle{
 private:
-    vector<double> alpha;
+    vector<long double> alpha;
 public:
-    Particle(vector<double>);
+    Particle(vector<long double>);
     Particle(int);
     Particle();
-    vector<double> getCoordinates();
-    double getCoordinate(int);
-    void putCoordinate (int, double);
+    vector<long double> getCoordinates();
+    long double getCoordinate(int);
+    void putCoordinate (int, long double);
     Particle operator+(Particle P);
     Particle operator*(Particle P);
     Particle operator*(int p);
     Particle operator-(Particle P);
     void Show();
     void draw();
+    bool equality();
 };
 class Swarm{
 private:
@@ -29,57 +33,47 @@ private:
     Particle greatBestParticle;
     int nIteration;
     int nSmena, nNullDelta;
-    double K0, K1, K2;
+    long double K0, K1, K2;
 public:
     Swarm(int nCoordinates, int nParticle);
     int iteration();
     Particle getGreatBest(){return greatBestParticle;};
     int getNIteration(){return nIteration;};
 };
-const bool operator== (Particle P, double p);
-const bool operator== (double p, Particle P);
+const bool operator== (Particle P, long double p);
+const bool operator== (long double p, Particle P);
 const bool operator== (Particle P1, Particle P2);
 const bool operator!= (Particle P1, Particle P2);
-const bool operator< (Particle P1, double p);
-const bool operator> (Particle P1, double p);
-double d(Particle P);
-const int nParticle = 100000 ;
-const int nCoordinates = 7;
+const bool operator< (Particle P1, long double p);
+const bool operator> (Particle P1, long double p);
+long double d(Particle P);
 void display();
 void timer(int);
-void plot(double(*) (double), double, double);
+void plot(long double(*) (long double), long double, long double);
 Swarm s1(nCoordinates, nParticle);
 int main(int argc, const char * argv[]) {
-    
-    srand((unsigned int)time(0));
-//    cout<<M_PI/2;
-//    getchar();
-    
+    srand(time(0));
     glutInitDisplayMode(GLUT_RGB);
     glutInitWindowSize(700, 300);
     glutCreateWindow("Рой");
-    
     glClearColor(0.2, 0.2, 0.2, 1);
     glClear(GL_COLOR_BUFFER_BIT);
     glOrtho(-M_PI/2, M_PI, -2, 2, 0, 1);
-    
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glEnable(GL_BLEND);
     glEnable(GL_POINT_SMOOTH);
     glHint(GL_POINT_SMOOTH_HINT, GL_NICEST);
     glEnable(GL_LINE_SMOOTH);
     glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
-    
     glutDisplayFunc(display);
     glutTimerFunc(1, timer, 0);
     glutMainLoop();
-    
     return 0;
 }
-void plot(double (*f) (double),double a, double b){
+void plot(long double (*f) (long double),long double a, long double b){
     glColor3f(1, 0, 0);
     glBegin(GL_LINE_STRIP);
-    for (double i = a; i < b; i += 0.01)
+    for (long double i = a; i < b; i += 0.01)
         glVertex2d(i, f(i));
     glEnd();
     glColor3f(1.0, 1.0, 1.0);
@@ -96,14 +90,13 @@ void plot(double (*f) (double),double a, double b){
     glVertex2d(M_PI/2,2);
     glEnd();
     glDisable(GL_LINE_STIPPLE);
-    
 }
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT);
     plot(sin, -M_PI, M_PI);
     s1.getGreatBest().draw();
-    if (d(s1.getGreatBest())>pow(10,-6)){
+    if (d(s1.getGreatBest())>eps){
         if (s1.iteration() == 10){
             s1 = Swarm(nCoordinates,nParticle);
         }
@@ -113,22 +106,20 @@ void display()
         getchar();
     }
     glutSwapBuffers();
-//    sleep(0.25);
 };
 void timer(int A)
 {
     glutPostRedisplay();
-    if (d(s1.getGreatBest())>pow(10,-6))
+    if (d(s1.getGreatBest())>eps)
         glutTimerFunc(20, timer, 0);
 }
 Swarm::Swarm(int nCoordinates, int nParticle){
     cout << "Создание роя: \n";
-    
     for (int i=0; i< nParticle; i++){
-        vector<double> tmp(nCoordinates);
+        vector<long double> tmp(nCoordinates);
         for (int j=0;j<nCoordinates;j++){
             
-            tmp.at(j) =(double)rand()/RAND_MAX*M_PI/2;
+            tmp.at(j) =(long double)rand()/RAND_MAX*M_PI/2;
         }
         Part.push_back(*new Particle(tmp));
         best.push_back(Part[i]);
@@ -147,11 +138,10 @@ Swarm::Swarm(int nCoordinates, int nParticle){
     nIteration = 0;
     nSmena = 0;
     nNullDelta = 0;
-    K0 = 0.9; K1 = 1.9; K2 = 1.9;
+    K0 = 0.8; K1 = 1.0; K2 = 1.0;
 }
-
 int Swarm::iteration(){
-    vector<double> tmp(nCoordinates,0);
+    vector<long double> tmp(nCoordinates,0);
     Particle deltaX(tmp);
     cout<<"Итерация № \007"<<nIteration+1<<endl;
     for (int i=0; i<nParticle; i++) { //Particles
@@ -169,7 +159,6 @@ int Swarm::iteration(){
             if (Part[i].getCoordinate(j) > M_PI/2 ){
                 Part[i].putCoordinate(j, Part[i].getCoordinate(j)-M_PI/2);
             };
-            
         }
         if (d(Part[i]) < d(best[i])){
             best[i] = Part[i];
@@ -189,16 +178,15 @@ int Swarm::iteration(){
         best[i].Show();
         cout<<"D(Great) "<<d(greatBestParticle)<<endl;
         cout<<endl;
-        
+        if (Part[i].equality()){
+            s1 = Swarm(nCoordinates, nParticle);
+            return 0;
+        }
     }
     nIteration++;
     nSmena++;
-//    if (nIteration == 10)
-//        K0 = 0.6;
-//    if (nIteration == 12)
-//        K0 = 0.4;
-    if (d(greatBestParticle)<pow(10,-5 )) {
-        K0 = 0.2;
+    if (d(greatBestParticle)<pow(10,-4 )) {
+        K0 = 0.01;
     }
     if (d(greatBestParticle)< 5* pow(10,-6)) {
         K0 = 0.001;
@@ -206,11 +194,16 @@ int Swarm::iteration(){
     cout<<"D(Great) "<<d(greatBestParticle)<<endl;
     cout<<"greatBest: ";
     greatBestParticle.Show();
-    if ((nSmena>=5*nCoordinates)&&(nNullDelta >= nCoordinates))
+    if ((nSmena>=5*nCoordinates)&&(nNullDelta >= nCoordinates)){
+        printf("\e[1;1H\e[2J");
+        system("clear");
+        cout<<"clear"<<endl<<endl;
         return 10;
+        
+    }
     return 0;
 }
-const bool operator<(Particle P, double p){
+const bool operator<(Particle P, long double p){
     for (int i=0; i<P.getCoordinates().size(); i++) {
         if (!(P.getCoordinate(i) < p)) {
             return false;
@@ -218,7 +211,7 @@ const bool operator<(Particle P, double p){
     }
     return true;
 }
-const bool operator>(Particle P, double p){
+const bool operator>(Particle P, long double p){
     for (int i=0; i<P.getCoordinates().size(); i++) {
         if (!(P.getCoordinate(i) > p)) {
             return false;
@@ -226,10 +219,10 @@ const bool operator>(Particle P, double p){
     }
     return true;
 }
-const bool operator== (Particle P, double p){
+const bool operator== (Particle P, long double p){
     return (P - p) < pow(10, -6);
 }
-const bool operator== (double p, Particle P){
+const bool operator== (long double p, Particle P){
     return P==p;
 }
 const bool operator== (Particle P1, Particle P2){
@@ -243,18 +236,18 @@ const bool operator== (Particle P1, Particle P2){
 const bool operator!= (Particle P1, Particle P2){
     return !(P1==P2);
 }
-void Particle::putCoordinate(int n, double p){
+void Particle::putCoordinate(int n, long double p){
     alpha.at(n) = p;
 }
-double Particle::getCoordinate(int n){
+long double Particle::getCoordinate(int n){
     return alpha.at(n);
 }
-vector<double> Particle::getCoordinates(){
+vector<long double> Particle::getCoordinates(){
     return alpha;
 }
 Particle::Particle(int nCoordinates){
     for (int i = 0; i < nCoordinates; i++)
-        alpha.push_back((double)rand()/RAND_MAX*1.5);
+        alpha.push_back((long double)rand()/RAND_MAX*1.5);
 }
 Particle::Particle(){};
 void Particle::Show(){
@@ -264,7 +257,7 @@ void Particle::Show(){
     }
     cout<<endl;
 }
-Particle::Particle(vector<double> TMP){
+Particle::Particle(vector<long double> TMP){
     alpha = TMP;
 }
 Particle Particle::operator-(Particle P){
@@ -280,7 +273,7 @@ Particle Particle::operator-(Particle P){
         numberMax = P.alpha.size();
         MaxMin = false;
     }
-    vector<double> result(numberMax,0.0);
+    vector<long double> result(numberMax,0.0);
     
     for (int i = 0; i<numberMin; i++){
         result[i]=alpha[i]-P.alpha[i];
@@ -288,7 +281,7 @@ Particle Particle::operator-(Particle P){
     return Particle(result);
 }
 Particle Particle::operator*(int p){
-    vector<double> result(alpha.size());
+    vector<long double> result(alpha.size());
     for (int i=0; i<alpha.size(); i++){
         result[i]=alpha[i] * p;
     }
@@ -307,7 +300,7 @@ Particle Particle::operator*(Particle P){
         numberMax = P.alpha.size();
         MaxMin = false;
     }
-    vector<double> result(numberMax,0.0);
+    vector<long double> result(numberMax,0.0);
     
     for (int i = 0; i<numberMin; i++){
         result[i] = alpha.at(i)*P.alpha.at(i);
@@ -328,7 +321,7 @@ Particle Particle::operator+(Particle P){
         numberMax = P.alpha.size();
         MaxMin = false;
     }
-    vector<double> result(numberMax,0.0);
+    vector<long double> result(numberMax,0.0);
     
     for (int i = 0; i<numberMin; i++){
         result[i] = alpha[i]-P.alpha[i];
@@ -346,16 +339,23 @@ void Particle::draw(){
         
     }
 }
-double d(Particle P){
-    double tmp = 0.0;
-    for (int i=1; i<=P.getCoordinates().size(); i++){
-        double tmp1 = 0.0;
-        for (int j=1; j<=P.getCoordinates().size(); j++){
-            tmp1 += cos((2*i+1) * P.getCoordinate(j-1));
-        }
-        tmp += (tmp1/(2*i+1))*(tmp1/(2*i+1));
+long double d(Particle P){
+    
+    long double tmp = 0.0;
+        for (int i=1; i<=P.getCoordinates().size(); i++){
+            long double tmp1 = 0.0;
+            for (int j=1; j<=P.getCoordinates().size(); j++){
+                tmp1 += cos((2*i+1) * P.getCoordinate(j-1));
+            }
+            tmp += tmp1*tmp1/((2*i+1)*(2*i+1));
     }
     return tmp;
 }
-
-
+bool Particle::equality(){
+    for (int i=1; i<alpha.size(); i++) {
+        if (abs(alpha.at(i-1) - alpha.at(i))<pow(10, -5)){
+            return true;
+        }
+    }
+    return false;
+}
